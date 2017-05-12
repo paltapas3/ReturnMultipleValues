8d9eefe192bc8ae60891b1caf217bac3ecb2ddec
paltapas3 <paltapas3@gmail.com>
2017-05-13 00:42:08 +0530

    Commiting all project changes

---
+++ MinMaxValue.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestProject
+{
+
+    public struct StructMinMax
+    {
+        public int min;
+        public int max;
+    }
+
+    public class MinMaxValue
+    {
+        public int min { get; set; }
+        public int max { get; set; }
+        public void MultipleReturnsValue(int x, int y, ref int maxvalue, ref int minvalue)
+        {
+            minvalue = x < y ? x : y;
+            maxvalue = x > y ? x : y;
+      
+        }
+        public void OutMultipleReturnsValue(int x, int y, out int maxvalue, out int minvalue)
+        {
+            minvalue = x < y ? x : y;
+            maxvalue = x > y ? x : y;
+            
+        }
+
+        public int[] ArrayMultipleReturnsValue(int x, int y)
+        {
+            int[] minMax = new int[2];
+            minMax[1] = x < y ? x : y;
+            minMax[0] = x > y ? x : y;
+            return minMax;
+        }
+
+        public StructMinMax StructMultipleReturnsValue(int x, int y)
+        {
+            StructMinMax values = new StructMinMax();
+            values.min = x < y ? x : y;
+            values.max = x > y ? x : y;
+            return values;
+        }
+
+        public Tuple<int, int> TupleMultipleReturnsValue(int x, int y)
+        {
+            int minvalue, maxvalue;
+            minvalue = x < y ? x : y;
+            maxvalue = x > y ? x : y;
+            return new Tuple<int, int>(minvalue, maxvalue);
+        }
+
+        public ArrayList ArrayListultipleReturnsValue(int x, int y)
+        {
+            ArrayList array = new ArrayList();
+            array.AddRange(new int[] { 0, 0 });
+            array[1] = x < y ? x : y;
+            array[0] = x > y ? x : y;
+            return array;
+        }
+
+        public MinMaxValue ObjectMultipleReturnsValue(int x, int y)
+        {
+            MinMaxValue objMinMaxValue = new MinMaxValue();
+            if (x > y)
+            {
+                objMinMaxValue.max = x;
+                objMinMaxValue.min = y;
+            }
+            else
+            {
+                objMinMaxValue.max = y;
+                objMinMaxValue.min = x;
+            }
+            return objMinMaxValue;
+
+        }
+    }
+
+}
---
+++ Multivaluereturn.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+   
+    public class Multivaluereturn
+    {
+                
+        static void Main(string[] args)
+        {
+            int x = 10, y = 20, max = 0, min = 0;
+            MinMaxValue objMinMax = new MinMaxValue();
+
+            // Ref example  
+            Console.WriteLine("Return Ref");
+            objMinMax.MultipleReturnsValue(x, y, ref max, ref min);
+            Console.WriteLine("Minimum Value: " + min);
+            Console.WriteLine("Maximum Value: " + max);
+            Console.ReadLine();
+
+            // Out example
+            Console.WriteLine("Return Out");
+            objMinMax.OutMultipleReturnsValue(x, y, out max, out min);
+            Console.WriteLine("Minimum Value: " + min);
+            Console.WriteLine("Maximum Value: " + max);
+            Console.ReadLine();
+
+            // Array example
+            Console.WriteLine("Return Array");
+            int[] minMax = objMinMax.ArrayMultipleReturnsValue(x, y);
+            Console.WriteLine("Minimum Value: " + minMax[1]);
+            Console.WriteLine("Maximum Value: " + minMax[0]);
+            Console.ReadLine();
+
+            //ArrayList example 
+            Console.WriteLine("Return ArrayList");
+            ArrayList ar = new ArrayList();
+            ar = objMinMax.ArrayListultipleReturnsValue(x, y);
+            Console.WriteLine("Minimum Value: " + ar[1]);
+            Console.WriteLine("Maximum Value: " + ar[0]);
+            Console.ReadLine();
+
+            // Return Struct 
+            Console.WriteLine("Return Struct");
+            StructMinMax results = objMinMax.StructMultipleReturnsValue(x, y);
+            Console.WriteLine("Minimum Value: " + results.min);
+            Console.WriteLine("Maximum Value: " + results.max);
+            Console.ReadLine();
+
+            // Return Tupple 
+            Console.WriteLine("Return Tupple");
+            var tuple = objMinMax.TupleMultipleReturnsValue(x, y);
+            Console.WriteLine("Minimum Value: " + tuple.Item1);
+            Console.WriteLine("Maximum Value: " + tuple.Item2);
+            Console.ReadLine();
+
+
+            // Return Object 
+            Console.WriteLine("Return Object");
+            MinMaxValue objMinnax = new MinMaxValue();
+            objMinnax = objMinMax.ObjectMultipleReturnsValue(x, y);
+            Console.WriteLine("Minimum Value: " + objMinnax.min);
+            Console.WriteLine("Maximum Value: " + objMinnax.max);
+            Console.ReadLine();
+
+        }
+        
+
+}
+}
